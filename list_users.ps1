0a68d8c17cab6cf03beed0f0445b394b509bf05e
Deepak <102180983+karkideepak@users.noreply.github.com>
2026-03-20 09:53:53 -0500

    Update list_users.ps1

--- list_users.ps1
+++ list_users.ps1
@@ -3,3 +3,6 @@ Get-LocalUser
 
 Get-LocalUser | Select-Object Name, Enabled, LastLogon
 #To display only the name, enabled status, and last logon time
+
+Get-LocalUser -Name "username"
+# details for a specific local user
